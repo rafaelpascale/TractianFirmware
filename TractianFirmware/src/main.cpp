5b5b8522b3ca58d0e62a4c8688c8c80562206e86
Rafael <rafael.pascale@zasso.com>
2022-04-07 17:54:27 -0300

    DHT e Lora

--- TractianFirmware/src/main.cpp
+++ TractianFirmware/src/main.cpp
@@ -1,34 +1,52 @@
 #include <Arduino.h>
+//#define USE_LORA
+//#define USE_DHT
+
+
+#ifdef USE_DHT
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#endif
 
+#ifdef USE_LORA
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>
+#define SCK 5   // GPIO5  SCK
+#define MISO 19 // GPIO19 MISO
+#define MOSI 27 // GPIO27 MOSI
+#define SS 18   // GPIO18 CS
+#define RST 14  // GPIO14 RESET
+#define DI00 26 // GPIO26 IRQ(Interrupt Request)
+#define BAND 433E6 //Frequência do radio - exemplo : 433E6, 868E6, 915E6
+#endif
+
+#ifdef USE_DHT
 #define DHTPIN 35     // Digital pin connected to the DHT sensor 
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
-
-// Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
 #define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
-
 DHT_Unified dht(DHTPIN, DHTTYPE);
+#endif
+
+
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
+#include "images.h"            // Imagem da Tractian 
 
 uint32_t delayMS;
 
 void setup() {
   Serial.begin(9600);
+   #ifdef USE_DHT
   dht.begin();
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
   dht.humidity().getSensor(&sensor);
   delayMS = sensor.min_delay / 1000;
+  #endif
 }
-
 void loop() {
+
+  #ifdef USE_DHT
   // Delay between measurements.
   delay(delayMS);
   // Get temperature event and print its value.
@@ -54,4 +72,5 @@ void loop() {
     Serial.print(umidade);
     Serial.println(F("%"));
   }
+  #endif
 }
