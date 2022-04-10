96739f48a3693e413831d8fb5eb9a7407d14667c
Rafael <rafael.pascale@zasso.com>
2022-04-09 21:49:41 -0300

    Mandando os dados de Temperatura e Umidade por BLE

--- TractianFirmware/src/main.cpp
+++ TractianFirmware/src/main.cpp
@@ -9,13 +9,14 @@ Empresa Tracitan
 
 #include <Arduino.h>
 #include <WiFi.h>
-#define ESP_getChipId()   ((uint32_t)ESP.getEfuseMac())
+
 
 #define USE_LORA
 #define USE_DHT
 #define USE_DISPLAY
+#define USE_BLE
 
-
+#define ESP_getChipId()   ((uint32_t)ESP.getEfuseMac())
 #ifdef USE_DHT
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
@@ -40,6 +41,13 @@ Empresa Tracitan
 #include "images.h"             // Imagem do Logo da Tractian
 #endif
 
+#ifdef USE_BLE
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#endif
+
 #ifdef USE_DHT
 #define DHTPIN 35     // Digital pin connected to the DHT sensor 
 #define DHTTYPE    DHT22     // DHT 22 (AM2302)
@@ -62,6 +70,8 @@ void sendPacket()
   Serial.println("Pacote Enviado desse modelo + temperatura + umidade"); 
 }
 #endif
+
+
 
 #ifdef USE_DISPLAY
 #define SDA 21   // GPIO5  SCK
@@ -91,11 +101,82 @@ void drawInicio() {
 }
 
 #endif
+
+
+
+#ifdef USE_BLE
+BLECharacteristic *characteristicTX; //através desse objeto iremos enviar dados para o client
+bool deviceConnected = false; //controle de dispositivo conectado
+
+#define SERVICE_UUID           "ab0828b1-198e-4351-b779-901fa0e0371e" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "4ac8a682-9736-4e5d-932b-e9b31405049c"
+#define CHARACTERISTIC_UUID_TX "0972EF8C-7613-4075-AD52-756F33D4DA91"
+
+
+//callback para receber os eventos de conexão de dispositivos
+class ServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+//callback  para envendos das características
+class CharacteristicCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *characteristic) {
+      //retorna ponteiro para o registrador contendo o valor atual da caracteristica
+      std::string rxValue = characteristic->getValue(); 
+      //verifica se existe dados (tamanho maior que zero)
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+
+        for (int i = 0; i < rxValue.length(); i++) {
+          Serial.print(rxValue[i]);
+        }
+
+        Serial.println();
+
+        // Do stuff based on the command received
+        if (rxValue.find("L1") != -1) { 
+          Serial.print("Turning LED ON!");
+
+        }
+        else if (rxValue.find("L0") != -1) {
+          Serial.print("Turning LED OFF!");
+
+        }
+        // Do stuff based on the command received from the app
+        else if (rxValue.find("B1") != -1) { 
+          Serial.print("Turning Buzzer ON!");
+
+        }
+        else if (rxValue.find("B0") != -1) {
+          Serial.print("Turning Buzzer OFF!");
+
+        }
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+#endif
+
+
+
+
+
 uint32_t delayMS;
 unsigned long previousMillis = 0;
 
 /*
+************************************************************************************************************
 ********************************************SETUP***********************************************************
+************************************************************************************************************
 */
 void setup() {
   Serial.begin(9600);
@@ -123,11 +204,39 @@ void setup() {
   dht.humidity().getSensor(&sensor);
   delayMS = sensor.min_delay / 1000;
   #endif
+
+  #ifdef USE_BLE
+  BLEDevice::init("Tractian"); // nome do dispositivo bluetooth  
+  BLEServer *server = BLEDevice::createServer(); //cria um BLE server 
+  server->setCallbacks(new ServerCallbacks()); //seta o callback do server
+  BLEService *service = server->createService(SERVICE_UUID);
+  // Create a BLE Characteristic para envio de dados
+  characteristicTX = service->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+                      
+  characteristicTX->addDescriptor(new BLE2902());
+
+  // Create a BLE Characteristic para recebimento de dados
+  BLECharacteristic *characteristic = service->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  characteristic->setCallbacks(new CharacteristicCallbacks());
+  // Start the service
+  service->start();
+  // Start advertising (descoberta do ESP32)
+  server->getAdvertising()->start();
+  #endif
 }
 
 
 /*
+***********************************************************************************************************
 ********************************************LOOP***********************************************************
+***********************************************************************************************************
 */
 
 void loop() {
@@ -171,6 +280,7 @@ void loop() {
     #ifdef USE_DISPLAY
     if (currentMillis - previousMillis >= 3000){
     previousMillis = currentMillis;
+    display.clear();
      drawData();
       }
    #endif
@@ -178,6 +288,23 @@ void loop() {
   #ifdef USE_LORA
    for (int i = 0; i <= 10; i++) {
    sendPacket();}
+  #endif
+
+  #ifdef USE_BLE
+  if (deviceConnected) {
+ // Convetendo um valor para uma array
+  char tString[8]; 
+  dtostrf(temperatura, 1, 2, tString); // float_val, min_width, digits_after_decimal, char_buffer
+  char uString[8]; 
+  dtostrf(umidade, 1, 2, uString); // float_val, min_width, digits_after_decimal, char_buffer
+
+  characteristicTX->setValue(tString + "C"); //seta o valor que a caracteristica notificará (enviar)       
+  characteristicTX->notify(); // Envia o valor para o smartphone  
+
+  characteristicTX->setValue(uString); //seta o valor que a caracteristica notificará (enviar)       
+  characteristicTX->notify(); // Envia o valor para o smartphone 
+
+  }
   #endif
 
 }
