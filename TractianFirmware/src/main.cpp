6127fd2a4795c931fc6b18da79a0cc3d173f5537
Rafael <rafael.pascale@zasso.com>
2022-04-09 22:30:44 -0300

    Atualizando o Deep sleep

--- TractianFirmware/src/main.cpp
+++ TractianFirmware/src/main.cpp
@@ -188,7 +188,13 @@ void print_wakeup_reason(){
 
   switch(wakeup_reason)
   {
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break; 
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  
     }
 }
 
@@ -334,5 +340,6 @@ void loop() {
 
   }
   #endif
+  
   esp_deep_sleep_start();
 }
